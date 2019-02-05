03c85e4dd48fc948a27237d350d8b26c40fd0120
Jorge Vazquez <jorvazper.kuki@gmail.com>
2019-02-05 11:07:50 +0100

    Archivos iniciales

---
+++ rutina_clock.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef RUTINA_CLOCK_H_   /* Include guard */
+#define RUTINA_CLOCK_H_  
+
+void start_counter();
+double get_counter();
+double mhz();
+
+#endif
+
