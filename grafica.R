1bb1f704c3394afcff1de772a403bc9fdc2f35e9
Jorge Vazquez <jorvazper.kuki@gmail.com>
2019-02-17 01:13:34 +0100

    Incluir script de R

---
+++ grafica.R
@@ -0,0 +1,24 @@
+
+
+resultados = read.table("resultados.txt", header = TRUE)
+
+resultados$fallos = (resultados$fallos)/10000 
+
+head(resultados)
+
+filter(resultados, hilos == 2 & numero == 2000)$fallos
+
+#install.packages("dplyr")
+library("dplyr")
+
+nombres= list(x = c(seq(2000, 20000, by=2000)),  z = c(seq(2, 12, by=2))) 
+datos = matrix (nrow = 10, ncol = 6, dimnames = nombres)
+
+for (i in c(seq(2, 12, by=2))) {
+  for (j in c(seq(2000, 20000, by=2000)))
+    datos[j/2000, i/2] = filter(resultados, hilos == i & numero == j)$fallos
+}
+
+barplot(datos, col=c("black", "brown3", "darkgoldenrod3", "dodgerblue3", "darkgrey", 
+                     "darkgoldenrod4", "darkorange3", "darkorchid3", "firebrick3", "deeppink3") , border="black", 
+        font.axis=2, beside=T, legend=rownames(data), xlab="Hilos", ylab="Porcentaje", font.lab=2)
